6b65dfab32f1df27551084908da40e1d43bdcd9e
nik-100xdevz <rainikhil760@gmail.com>
2026-01-15 18:46:47 +0530

    chnages in background

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -113,7 +113,7 @@ export const HomePage: React.FC = () => {
       </section>
 
       {/* CTA Section */}
-      <section className="bg-gradient-to-r from-purple-600 to-blue-600 text-white py-20">
+      <section className=" bg-gradient-to-r from-violet-200 to-pink-200  text-white py-20">
         <div className="max-w-4xl mx-auto text-center px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl md:text-4xl font-bold mb-6">
             Ready to Start Your Journey?
